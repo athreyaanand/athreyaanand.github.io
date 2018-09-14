dd3a4eda1711209a251060468570f5c1e72f7289
Athreya Anand <31478366+athreyaanand@users.noreply.github.com>
2018-09-14 02:27:35 -0400

    COMPLETE REVAMP SPECTACULAR

---
+++ scripts/main.js
@@ -0,0 +1,50 @@
+'use strict';
+
+$(document).ready(function() {
+  var randomColor, changeColor, colorT, scrollio;
+
+  colorT = 0;
+
+  randomColor = function() {
+    colorT = Math.floor(Math.random() * 10);
+  };
+  randomColor();
+
+  changeColor = function() {
+    document.body.className = 'color' + colorT % 10;
+    colorT++;
+    setTimeout(changeColor, 3000);
+  };
+  changeColor();
+
+  $.typer.options.highlightSpeed = 15;
+  $.typer.options.typeSpeed = 50;
+  $.typer.options.typerInterval = 6000;
+  // $.typer.options.typerInterval = 3000;
+  $.typer.options.typeDelay = 50;
+  $.typer.options.clearDelay = 1000;
+  $('[data-typer-targets]').typer();
+
+  scrollio = function(target, speed) {
+    $('html, body').animate({
+      scrollTop: $(target).offset().top
+    }, speed, 'easeOutExpo');
+  };
+
+  $('.workheader').on('click', function(e) {
+    var target;
+    e.preventDefault();
+    target = $(this).attr('href');
+    scrollio(target, 800);
+  });
+
+  $('[data-gtm-category]').on('click', function() {
+    var category = $(this).data('gtm-category');
+    var action = $(this).data('gtm-action');
+    var label = $(this).data('gtm-label');
+    gtmClickTrack(category, action, label);
+  });
+
+  console.log('%c stop tryna steal my stuff ✌', 'font-size:10em;');
+
+});
